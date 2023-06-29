402d3670a9bbcf5294f4ac486e89ff3df7e7e7d3
Ignacio Grillo Caimary <igrilloc@pop-os.localdomain>
2023-06-29 18:44:11 -0300

    first commit

---
+++ src/components/Movies.jsx
@@ -0,0 +1,33 @@
+function ListOfMovies ({ movies }) {
+  
+  return (
+    <ul className='movies'>
+      {
+        movies.map((movie) => (
+          <li className='movie' key={movie.id}>
+            <img src={movie.image} alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>{movie.year}</p>
+          </li>
+        ))
+      }
+    </ul>
+  );
+
+}
+
+function NoMoviesResults () {
+  return (<p>No se encontraron películas para esta búsqueda</p>);
+}
+
+export function Movies ({ movies }) {
+  
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies
+    ? <ListOfMovies movies={movies} />
+    : <NoMoviesResults />
+  );
+
+}
